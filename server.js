ba84242385af2b0299a1cc6aa8b69498ceaa31cd
Phil Pepin <ppepin34@gmail.com>
2022-05-15 13:57:21 -0400

    start building server file because duh

---
+++ server.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { notes } = require('./db/db.json');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// get js and css files from public
+app.use(express.static('public'));
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
