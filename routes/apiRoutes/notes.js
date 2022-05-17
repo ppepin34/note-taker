6b74447965fca4aa5a968cc45c1c06a181f8cf95
Phil Pepin <ppepin34@gmail.com>
2022-05-16 22:58:53 -0400

    donesky

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-const { append } = require('express/lib/response');
 const { notes } = require('../../data/db.json');
-const { validateNote, createNewNote, findById }  = require('../../lib/notes')
+const { validateNote, createNewNote, removeNote } = require('../../lib/notes')
 
 router.get('/notes', (req, res) => {
     let results = notes;
@@ -11,8 +10,16 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
 
+    if (notes.length === 0) {
+        req.body.id = 0;
+    } else {
+        idArray = notes.map(x => x.id)
+
+        max = Math.max(...idArray);
+
+        req.body.id = max + 1;
+    }
     // if any data in the req.body is incorrect, send 400 error back
     if (!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
@@ -24,8 +31,7 @@ router.post('/notes', (req, res) => {
 
 router.delete('/notes/:id', (req, res) => {
     // get object that is to be deleted
-    const deleteObj = findById(req.params.id, notes);
-
-    const result = deleteNote(deleteObj);
+    note = removeNote(req.params.id, notes);
+    res.json(note);
 })
 module.exports = router;
